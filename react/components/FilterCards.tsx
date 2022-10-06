2c14c440fdda95e78644f7dd51ad36616104c1d4
daniel17110290 <danielfdz0290@gmail.com>
2022-10-05 22:23:46 -0500

    feat: agregando componente custom con estilos

--- react/components/FilterCards.tsx
+++ react/components/FilterCards.tsx
@@ -1,11 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import { useQuery } from "react-apollo"
 import Query_value from "../graphql/getDepartmentGroup.graphql"
+import FilterDepartment from "./FilterDepartment"
+// import FilterCategories from "./FilterCategories"
 
 const FilterCards = () => {
   const { data, loading } = useQuery(Query_value)
   console.log("mis datos de query son:", data, loading)
-  return <div>Filtros departamantos</div>
+  const [slug, setSlug] = useState("")
+  console.log("mi slug seleccionado es:", slug)
+
+  return loading
+    ?
+    <div>loading...</div>
+    :
+    <>
+      <div>
+        <FilterDepartment
+          departments={data?.categories}
+          handleSetSlug={setSlug}
+        />
+      </div>
+
+    </>
 }
 
 export default FilterCards
